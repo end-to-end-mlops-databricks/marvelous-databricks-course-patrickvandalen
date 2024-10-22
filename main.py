46a449f0f116588b4a9c579d491c59290b6d7e5e
Patrick van Dalen <patrickvandalen@gmail.com>
2024-10-22 16:12:15 +0200

    Adding pre-commit

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-import yaml
 import logging
 import subprocess
 import sys
 
+import yaml
+
+
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-package = '/Volumes/mdl_europe_anz_dev/patrick_mlops/mlops_course/mlops_with_databricks-0.0.1-py3-none-any.whl'
+
+package = "/Volumes/mdl_europe_anz_dev/patrick_mlops/mlops_course/mlops_with_databricks-0.0.1-py3-none-any.whl"
 install(package)
 
 from hotel_reservations.data_processor import DataProcessor
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
 # Load configuration
-with open('project_config.yml', 'r') as file:
+with open("project_config.yml", "r") as file:
     config = yaml.safe_load(file)
 
 print("Configuration loaded:")
 print(yaml.dump(config, default_flow_style=False))
 
 # Initialize DataProcessor
-data_processor = DataProcessor('data/data.csv', config)
+data_processor = DataProcessor("data/data.csv", config)
 logger.info("DataProcessor initialized.")
 
 # Preprocess the data
