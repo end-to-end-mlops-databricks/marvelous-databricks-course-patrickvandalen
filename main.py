d1202597754278f20262a654282fac92b1229a3b
Patrick van Dalen <patrickvandalen@gmail.com>
2024-10-27 23:01:46 +0100

    Update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
-
 import yaml
+import json
+import mlflow
+from pyspark.sql import SparkSession
 
 # import subprocess
 # import sys
@@ -8,8 +10,10 @@ import yaml
 #     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 # package = "/Volumes/mdl_europe_anz_dev/patrick_mlops/mlops_course/mlops_with_databricks-0.0.1-py3-none-any.whl"
 # install(package)
-from hotel_reservations.data_processor import DataProcessor
 
+from src.hotel_reservations.data_processor import DataProcessor
+
+spark = SparkSession.builder.getOrCreate()
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
@@ -27,3 +31,53 @@ logger.info("DataProcessor initialized.")
 # Preprocess the data
 data_processor.preprocess_data()
 logger.info("Data preprocessed.")
+
+# Split Train and Test data
+train_set, test_set = data_processor.split_data()
+logger.info("Train and Test data splitted.")
+
+# Save data to catalog
+data_processor.save_to_catalog(train_set=train_set, test_set=test_set, spark=spark)
+logger.info("Data saved to catalog.")
+
+# Create mlflow experiment
+mlflow.set_tracking_uri("databricks")
+mlflow.set_experiment("/Shared/Hotel_Reservations")
+mlflow.set_experiment_tags({"repository_name": "Patrick_MLOps"})
+
+experiments = mlflow.search_experiments(
+    filter_string="tags.repository_name='Patrick_MLOps'"
+)
+
+print(experiments)
+
+# Write mlflow experiment to json file
+with open("mlflow_experiment.json", "w") as json_file:
+    json.dump(experiments[0].__dict__, json_file, indent=4)
+
+# Start MLFlow run
+with mlflow.start_run(
+    run_name="demo_run",
+    tags={"git_sha": "ffa63b430205ff7",
+          "branch": "week2"},
+    description="demo run",
+) as run:
+    mlflow.log_params({"type": "demo"})
+    mlflow.log_metrics({"metric1": 1.0, "metric2": 2.0})
+
+# Get Run info
+run_id = mlflow.search_runs(
+    experiment_names=["/Shared/Hotel_Reservations"],
+    filter_string="tags.git_sha='ffa63b430205ff7'",
+).run_id[0]
+
+run_info = mlflow.get_run(run_id=f"{run_id}").to_dictionary()
+
+print(run_info)
+print(run_info["data"]["metrics"])
+print(run_info["data"]["params"])
+
+# Write run info to json file
+with open("run_info.json", "w") as json_file:
+    json.dump(run_info, json_file, indent=4)
+
--- src/hotel_reservations/data_processor.py
+++ src/hotel_reservations/data_processor.py
@@ -1,10 +1,11 @@
 import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp, to_utc_timestamp
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-
 
 class DataProcessor:
     def __init__(self, filepath, config):
@@ -47,4 +48,28 @@ class DataProcessor:
         )
 
     def split_data(self, test_size=0.2, random_state=42):
-        return train_test_split(self.X, self.y, test_size=test_size, random_state=random_state)
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=test_size, random_state=random_state)
+        train_set = pd.concat([X_train, y_train], axis=1)
+        test_set = pd.concat([X_test, y_test], axis=1)
+        return train_set, test_set
+
+    def save_to_catalog(self, train_set: pd.DataFrame, test_set: pd.DataFrame, spark: SparkSession):
+        """Save the train and test sets into Databricks tables."""
+
+        train_set_with_timestamp = spark.createDataFrame(train_set).withColumn(
+            "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC"))   
+        
+        test_set_with_timestamp = spark.createDataFrame(test_set).withColumn(
+            "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC"))
+        
+        train_table_uc = self.config["catalog_name"] + "." + self.config["schema_name"] + "." + "train_set"
+        test_table_uc = self.config["catalog_name"] + "." + self.config["schema_name"] + "." + "test_set"
+
+        train_set_with_timestamp.write.mode("append").saveAsTable(train_table_uc)
+        test_set_with_timestamp.write.mode("append").saveAsTable(test_table_uc)
+
+        spark.sql(f"ALTER TABLE {train_table_uc} "
+          "SET TBLPROPERTIES (delta.enableChangeDataFeed = true);")
+        
+        spark.sql(f"ALTER TABLE {test_table_uc} "
+          "SET TBLPROPERTIES (delta.enableChangeDataFeed = true);")
